c681050929d94a072d8fcf9d386184592cbee96e
peleg745 <61710146+peleg745@users.noreply.github.com>
2021-12-28 20:12:40 +0200

    Update Client

--- Client.py
+++ Client.py
@@ -2,36 +2,37 @@ import struct
 import time
 import traceback
 from struct import *
-from socket import *
 import enum
-from msvcrt import getch
+import socket
+# from msvcrt import getch
 from threading import Thread
-import keyboard
+# import keyboard
 from scapy.all import *
 
 
 # CLIENT_IP = '172.1.0/24'
 # CLIENT_IP = get_if_addr("eth1") # 172.1.0 (eth1)
 # CLIENT_IP = '172.1.0/24'
-CLIENT_IP = gethostbyname(gethostname())  # 172.99.0 (eth2)
+CLIENT_IP = socket.gethostbyname(socket.gethostname())  # 172.99.0 (eth2)
 localPORTUDP = 13110
-localPORTTCP = 2042
+localPORTTCP = 2142
 BUFFER_SIZE = 2048
 MAGIC_COOKIE = 0xabcddcba
 MSG_TYPE = 0x2
 
 class Client:
     def __init__(self):
-        self.team_name = "PD2"
+        self.team_name = "PD1"
         self.ip = CLIENT_IP
-        self.udp_socket = socket(AF_INET, SOCK_DGRAM)
-        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.game_is_on = False
 
     def connect_to_server(self, server_ip, server_port):
         """
         This function sends a connection request to the server.
         """
+        self.tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) # as is
         self.tcp_socket.bind(('', localPORTTCP))
         self.tcp_socket.connect((server_ip, server_port))
         print("Connected to Server")
@@ -57,6 +58,7 @@ class Client:
         and then sends the Client's Team Name to the Server
         """
         print("Client Started, listening for offer requests...")
+        self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) # as is
         self.udp_socket.bind(('', localPORTUDP)) # bind socket to local port number ??
 
         while True:
@@ -99,32 +101,59 @@ class Client:
     #         continue
     #     print("stopped typing")
 
-    def keyboard_presser(self):
-        # print("trying pressing")
-        counter_press = 0
-        keyboard.on_press(self.send_to_server)
-        while self.game_is_on:
-            continue
-        print("stopped typing")
+    # def keyboard_presser(self):
+    #     # print("trying pressing")
+    #     counter_press = 0
+    #     keyboard.on_press(self.send_to_server)
+    #     while self.game_is_on:
+    #         continue
+    #     print("stopped typing")
 
+    # def game_play(self):
+    #     try:
+    #         keyboard_thread = Thread(target=self.keyboard_presser)
+    #         msg = self.tcp_socket.recv(2048).decode()
+    #         print(msg)  # game started
+    #         self.game_is_on = True
+    #         keyboard_thread.start()
+    #         msg = self.tcp_socket.recv(2048).decode()
+    #         self.game_is_on = False
+    #         keyboard_thread.join()
+    #         print(msg)  # game ended
+    #         print("Server disconnected, listening for offer requests...")
+    #         self.tcp_socket.close()
+    #         return
+    #     except:
+    #         # traceback.print_exc()
+    #         self.finish_run()
+    #         return
     def game_play(self):
-        try:
-            keyboard_thread = Thread(target=self.keyboard_presser)
-            msg = self.tcp_socket.recv(2048).decode()
-            print(msg)  # game started
-            self.game_is_on = True
-            keyboard_thread.start()
-            msg = self.tcp_socket.recv(2048).decode()
-            self.game_is_on = False
-            keyboard_thread.join()
-            print(msg)  # game ended
-            print("Server disconnected, listening for offer requests...")
-            self.tcp_socket.close()
-            return
-        except:
-            # traceback.print_exc()
-            self.finish_run()
-            return
+        """
+        This function is used when the client starts to play the game - and starts to press button on the keyboard
+        """
+        message = self.tcp_socket.recv(BUFFER_SIZE)
+        message = str(message, 'utf-8')
+        print(message)
+        message = None
+        while True:
+            try:
+                incoming_message, _, _ = select([self.tcp_socket],[],[],0.2)
+                if incoming_message:
+                    message = self.tcp_socket.recv(BUFFER_SIZE)
+            except:
+                pass
+            if not message:
+                os.system("stty raw -echo")
+                char_coming, _, _ = select([sys.stdin], [], [], 0.2)
+                if char_coming:
+                    key = sys.stdin.read(1)
+                    self.tcp_socket.send(key.encode())
+            else:
+                os.system("stty -raw echo")
+                message = str(message, 'utf-8')
+                print(message)
+                break
+        self.udp_socket.close()
 
 def main():
     while True:
